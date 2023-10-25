0c7f970cdcfe49643e4083e63dcac2227ec7664a
kenbondoc <149012922+kenbondoc@users.noreply.github.com>
2023-10-25 14:29:08 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,9 +8,9 @@ slit.text('🥑🍞 Avocado Toast')
 slit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-slit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 # Display the table on the page.
 import pandas as pnda
 my_fruit_list = pnda.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+slit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 slit.dataframe(my_fruit_list)
 
