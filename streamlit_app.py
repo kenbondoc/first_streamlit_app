6310ada1a77acf34cdeab81df3433cc26f882cdd
kenbondoc <149012922+kenbondoc@users.noreply.github.com>
2023-10-25 14:27:06 -0700

    Update streamlit_app.py
    added pick list for fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,3 +10,7 @@ slit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 import pandas as pnda
 my_fruit_list = pnda.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 slit.dataframe(my_fruit_list)
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+slit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+# Display the table on the page.
