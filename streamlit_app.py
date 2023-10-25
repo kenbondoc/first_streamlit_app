e710f4275613c034463ceca25f9ae774ea10a5b9
kenbondoc <149012922+kenbondoc@users.noreply.github.com>
2023-10-25 14:28:07 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,10 +7,10 @@ slit.text('🐔 Hard-Boiled Free-Range Egg')
 slit.text('🥑🍞 Avocado Toast')
 slit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+# Let's put a pick list here so they can pick the fruit they want to include 
+slit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+# Display the table on the page.
 import pandas as pnda
 my_fruit_list = pnda.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 slit.dataframe(my_fruit_list)
 
-# Let's put a pick list here so they can pick the fruit they want to include 
-slit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-# Display the table on the page.
