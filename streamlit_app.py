dc385f3daf32b158118b293056fcfa593a867471
kenbondoc <149012922+kenbondoc@users.noreply.github.com>
2023-10-30 18:25:59 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,9 +64,9 @@ if slit.button('Get Fruit Load List'):
     slit.dataframe(my_data_row)
 
 # Allow end user to add a fruit to the list
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('Thanks for adding ', add_my_fruit)
-my_cur.execute("insert into fruit_load_list values ('from streamlit') ")
+add_my_fruit = slit.text_input('What fruit would you like to add?','jackfruit')
+slit.write('Thanks for adding ', add_my_fruit)
+my_cur.execute("insert into fruit_load_list values ('from slit') ")
 
 
 
