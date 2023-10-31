cb790384352e69e9ffbc0784dd4c8f0c5be26d7d
kenbondoc <149012922+kenbondoc@users.noreply.github.com>
2023-10-30 17:12:48 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,11 +25,16 @@ slit.dataframe(fruits_to_show)
 # New section to display fruityvice api response
 # import requests as rqst
 slit.header('Fruityvice Fruit Advice!')
-fruit_choice = slit.text_input('What fruit would you like information about?','Kiwi')
-slit.write('The user entered ', fruit_choice)
-fruityvice_response = rqst.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-fruityvice_normalized = pnda.json_normalize(fruityvice_response.json())
-slit.dataframe(fruityvice_normalized)
+try:
+  fruit_choice = slit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    slit.error("Please select a fruit to get information.")
+  else
+    fruityvice_response = rqst.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pnda.json_normalize(fruityvice_response.json())
+    slit.dataframe(fruityvice_normalized)
+except URLError as e:
+    streamlit.error()
 
 slit.stop()
 
