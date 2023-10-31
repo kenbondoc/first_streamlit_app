18d3759b935abd99c798e08a3ae268a207fcf193
kenbondoc <149012922+kenbondoc@users.noreply.github.com>
2023-10-30 17:14:11 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ try:
   fruit_choice = slit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     slit.error("Please select a fruit to get information.")
-  else
+  else:
     fruityvice_response = rqst.get("https://fruityvice.com/api/fruit/" + fruit_choice)
     fruityvice_normalized = pnda.json_normalize(fruityvice_response.json())
     slit.dataframe(fruityvice_normalized)
