e0942a36b47aa4f102ac87b131caeb6973477ca0
kenbondoc <149012922+kenbondoc@users.noreply.github.com>
2023-10-30 17:25:27 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 slit.dataframe(fruits_to_show)
 
 # create a repeatable code block (called a function)
-def get_fruityvice_data(this_fruit_choice):
+def get_fruityvice_data(fruit_choice):
     fruityvice_response = rqst.get("https://fruityvice.com/api/fruit/" + fruit_choice)
     fruityvice_normalized = pnda.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
@@ -36,7 +36,7 @@ try:
   if not fruit_choice:
     slit.error("Please select a fruit to get information.")
   else:
-    back_from_function = get_fruityvice_data(this_fruit_choice)
+    back_from_function = get_fruityvice_data(fruit_choice)
     slit.dataframe(back_from_function)
 except URLError as e:
     streamlit.error()
